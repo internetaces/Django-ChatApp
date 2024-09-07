cececc11e412105c757f640a78b28770abfab7c3
Marcel Zilkowski <info@internetaces.com>
2024-09-07 21:49:34 +0200

    Added chat to admin interface

--- first_django_app/chat/admin.py
+++ first_django_app/chat/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Message
 
+class MessageAdmin(admin.ModelAdmin):    
+    fields = ('text','created_at', 'author', 'receiver')    # Für Detailansicht
+    list_display = ('created_at', 'author', 'text', 'receiver')    # Für Überssichtsseite 
+    search_fields = ('text',) # Für Suche in Übersichtsseite
+    
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
