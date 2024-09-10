5ec08ef8b82dd8a3ecf6a02deb747d9f1cc08b8b
Marcel Zilkowski <info@internetaces.com>
2024-09-10 15:23:44 +0200

    Added message database object

--- first_django_app/chat/admin.py
+++ first_django_app/chat/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import Message
+from .models import Chat
 
 class MessageAdmin(admin.ModelAdmin):    
-    fields = ('text','created_at', 'author', 'receiver')    # Für Detailansicht
+    fields = ('chat','text','created_at', 'author', 'receiver')    # Für Detailansicht
     list_display = ('created_at', 'author', 'text', 'receiver')    # Für Überssichtsseite 
     search_fields = ('text',) # Für Suche in Übersichtsseite
     
 # Register your models here.
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Message, Chat
 
 def index(request):
+	if request.method == 'POST':
+		print('received data ' + request.POST['textmessage'])
+		myChat = Chat.objects.get(id=1)
+		Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
 	return render(request, 'chat/index.html', {'name':'Marcel'})
