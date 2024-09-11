34f9bc78016bcfbe46bcc663d3f20c4ec459109b
Marcel Zilkowski <info@internetaces.com>
2024-09-11 12:07:21 +0200

    Display chat messages

--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -3,7 +3,11 @@ from .models import Message, Chat
 
 def index(request):
 	if request.method == 'POST':
-		print('received data ' + request.POST['textmessage'])
+		print("Received data " + request.POST['textmessage'])
 		myChat = Chat.objects.get(id=1)
 		Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
-	return render(request, 'chat/index.html', {'name':'Marcel'})
+	chatMessages = Message.objects.filter(chat__id=1)
+	return render(request, 'chat/index.html', {'messages' : chatMessages})
+
+
+
